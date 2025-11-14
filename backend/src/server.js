c6ee64f087d537a8ef621c29c26554d3b24ecf85
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-14 20:38:08 +0500

    Add .gitignore, update environment configuration, and enhance server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,9 @@ import env from "./config/env.js";
 const app = express();
 const PORT = env.PORT;
 
+// middleware for parsing JSON bodies
+app.use(express.json());
+
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Server is running", status: "success" });
 });
